121a1fb421941c982b760d86fa95e2ce7c1fb343
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-10-02 17:25:41 -0300

    Adiciona procfile e port para rodar no heroku

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   console.log(process.env.MONGO_URI)
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
